ce9b5e119e8aa53dcd81fcb6aed7b480006c6980
Qazi Umer <qaziumer16@gmail.com>
2016-09-25 11:29:22 +0500

    Body Parser

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res, next){
     res.send("Timestamp Microservice");
 });
+
+app.get("/:unix", function(req, res, next){
+    res.send(req.params);
+});
+
+app.use(bodyParser.json());
+
 
 app.listen(PORT, function(){
 console.log("Server running on: " + PORT + "!");
